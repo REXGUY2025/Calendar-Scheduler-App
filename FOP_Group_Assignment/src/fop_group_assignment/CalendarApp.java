c163c7e1efc9ae2530e5b2e4edc2b37a54a75dae
aiman haikal-2025 <24002234@siswa.um.edu.my>
2026-01-09 17:47:19 +0800

    CalendarApp

---
+++ FOP_Group_Assignment/src/fop_group_assignment/CalendarApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package schedulecalendarapp;
+
+/**
+ *
+ * @author aiman
+ */
+public class CalendarApp {
+public static void main(String[] args) {
+new LoginFrame().setVisible(true);
+NotificationTimerService reminderService =
+new NotificationTimerService();
+reminderService.start();
+}
+}
