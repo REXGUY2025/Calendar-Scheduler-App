7c2695f0825f9f63c016fbfd52b426138ac6d429
aiman haikal-2025 <24002234@siswa.um.edu.my>
2026-01-19 00:13:05 +0800

    SAVE

--- FOP_Group_Assignment/src/fop_group_assignment/ConflictCheckerService.java
+++ FOP_Group_Assignment/src/fop_group_assignment/ConflictCheckerService.java
@@ -1,13 +1,14 @@
+<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package fop_group_assignment;
+=======
+>>>>>>> fede75459490d51bd5cf681a90faa0b936b89096
 
-/**
- *
- * @author aiman
- */
+package fop_group_assignment;
+
 import java.time.*;
 import java.util.*;
 
